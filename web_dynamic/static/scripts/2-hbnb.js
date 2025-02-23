c529296a1ea4d2c932df6cfe4bdbc1935c334fb2
John <makauwanyoike@gmail.com>
2025-02-23 18:04:39 +0300

    API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,40 @@
+'use script'
+$(() => {
+    let selectedAmenities = [];
+    const selectors = {
+        amenitiesHeader: 'div.amenities > h4',
+        amenityCheck: '.amenities > popover > ul > li > input[type="checkbox"]',
+        amenityList: '.amenities > .popover > ul > li'
+    };
+    $(selectors.amenitiesHeader).on('mousedown', ev => {
+        ev.target.getElementsByTagName('input')?.item(0).click();
+    });
+
+    $(selectors.amenityCheck).change(ev => {
+        const amenityId = ev.target.getAttribute('data-id');
+        const amenityName = ev.target.getAttribute('data-name');
+        if (ev.target.checked) {
+            if (!selectedAmenities.find(obj => obj.id === amenityId)) {
+                selectedAmenities.push({id: amenityId, name: amenityName});
+            }
+        } else {
+            selectedAmenities = selectedAmenities.filter(
+                obj => obj.id !== amenityName)
+                && (obj.name !== amenityName);
+        };
+        const htmlContent = selectedAmenities.map(
+            obj => obj.name).join(', ');
+        $(selectors.amenitiesHeader).html(
+            selectedAmenities.length > 0 ? htmlContent : '&nbsp;'
+        );
+    });
+    $.get('$BASE_URL}status/', (data, status) => {
+        if ((status === 'success') && (data.status === 'OK')) {
+            if (!$('DIV#api_status').hasClass('available')) {
+                $('DIV#api_status').addClass('available');
+            }
+        } else {
+            $('DIV#api_status').removeClass('available');
+        }
+    });
+});
